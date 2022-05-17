664d65dc33c2b97c1d095c3731098683bb9e19f2
cod <cod@haboob.sa>
2022-05-17 08:34:23 +0300

    latest change on python

--- primanota.py
+++ primanota.py
@@ -5,16 +5,19 @@ import sys
 import csv
 import json
 
+pnt_f = None
+pnr_f = None
+
 # parse_args: input argv, output dictionary of argv
 def parse_args(argv):
     argparser = argparse.ArgumentParser(epilog='primanota')
     argparser.add_argument('-i', '--input', help='Input File')
-    argparser.add_argument('-o', '--output', help='Output File')
+    argparser.add_argument('-o', '--output', help='Output Suffix')
     argparser.add_argument('-j', '--json', help='JSON schema rules')
 
     #print("parse_args parsing =>", argv)
     args = argparser.parse_args(argv)
-    
+
     args_dict = {
         'input': args.input,
         'output': args.output,
@@ -31,10 +34,10 @@ def parse_args(argv):
     if args.input is None:
         print('Errore: input richiesto.')
         fail = True
-    
+
     if fail:
         sys.exit(1)
-    
+
     return args_dict
 
 # load_csv: Read an input file, and return a list of dictonary with each line
@@ -47,21 +50,21 @@ def load_csv(csv_name):
 
         columns = next(reader)
         print(columns)
-        
+
         lines = 1
         for row in reader:
             lines = lines + 1
             line = {}
             i = 0
-            if len(row) == len(columns): 
+            if len(row) == len(columns):
                 for c in columns:
                     line[c]=row[i]
                     i = i + 1
-            
+
                 content.append(line)
             else:
                 print('Riga %d saltata %d %d' %(lines, len(row), len(columns)) )
-    
+
     return content
 
 def load_rules(json_name):
@@ -75,35 +78,84 @@ def load_rules(json_name):
     if len(data['rules']) == 0:
         print('Nessuna regola definita nel file')
         sys.exit(3)
-    
+
     print('Regole azienda %s caricate' %(data['codice_azienda']))
 
     f.close()
     return data
 
+def match_beginwith(string, value):
+    if string.startswith(value):
+        return True
+    return False
+
+def match_contains(string, value):
+    if value in string:
+        return True
+    return False
+
+def match_equals(string, value):
+    if string == value:
+        return True
+    return False
+
+def match_not(string, value):
+    if match_equals(string, value) and match_contains(string, value) and match_beginwith(string, value):
+        return False
+    return True
+
+def write_header(f, r):
+    print('Generazione %s' %(f))
+
+    with open(f, 'a+', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerow(r)
+
+def write_row(f, r):
+    with open(f, 'a+', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerow(r)
+
+# verify if r match something in line
+def match_rule(r, line):
+    if r is None:
+        return False
+    if line is None:
+        return False
+
+    content_to_evaluate = line[r['field']]    # content to evaluate
+    criteria = r['rules']
+    match_value = r['value']
+
+    if content_to_evaluate is None:
+        return False
+
+    if len(content_to_evaluate) == 0:
+        return False
+
+    options = {
+        "beginwith" : match_beginwith,
+        "contains" : match_contains,
+        "equals" : match_equals,
+        "not" : match_not
+    }
+
+    return options[criteria](content_to_evaluate, match_value)
+
 def match_line(testata, righe, rules, content):
-    
     lines = 0
 
     for line in content:
         for r in rules:
             process = False
 
-            if r['start'] is not None:
-                start = r['start']
-                field = line[r['field']]
+            match = match_rule(r, line)
 
-                if field.startswith(start):
-                    process = True
-            
-            if process:
-                for o_r in r['righe']:
-                    lines = lines + 1
-                    print(o_r)
+            if match:
+                riga_testata = prepara(testata, r['testata'], line)
+                write_row(pnt_f, testata, 
+                lines = lines + 1
 
-                
-                print(line[r['field']])
-    
     return lines
 
 def dict_csv(fields, values):
@@ -124,16 +176,22 @@ def primanota(args):
     content = load_csv(args['input'])
     desc = load_rules(args['json'])
 
-    linea_testata = desc['testate']['fields'].split(';')
-    linea_righe = desc['righe']['fields'].split(';')
+    pnt_f = 'pnt_' + args['output']
+    pnr_f = 'pnr_' + args['output']
+
+    header_testate = desc['testate']['fields'].split(';')
+    header_righe = desc['righe']['fields'].split(';')
+
+    write_header(pnt_f, header_testate)
+    write_header(pnr_f, header_righe)
 
     # definisce i set iniziali...
-    testata = dict_csv(linea_testata, desc['testate']['values'])
-    righe = dict_csv(linea_righe, desc['righe']['values'])
-     
+    testata = dict_csv(header_testate, desc['testate']['values'])
+    righe = dict_csv(header_righe, desc['righe']['values'])
+
     processed = match_line(testata, righe, desc['rules'], content)
     return processed
-    
+
 def main(argv=sys.argv[1:]):
     args = parse_args(argv)
     if args is None:
@@ -141,7 +199,6 @@ def main(argv=sys.argv[1:]):
 
     processed = primanota(args)
     print('Righe processate %d' %(processed))
-
 
 if __name__ == "__main__":
     main()
